813ea4384fe47eb864ba553222352cfe4fe11a41
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-02-24 18:26:28 +0530

    Feature: Add ability to create new user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   before_create :assign_guid
 
   has_one :detail, class_name: 'UserDetail'
+  accepts_nested_attributes_for :detail
 
   validates :name, presence: true
 
